8d12ed857a9a2967d96904eb68c5dc8c79806ebf
Islam Abdelkader <islam.abdelkader@lendinvest.com>
2023-01-10 18:38:39 +0200

    trpc setup

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+
+const appRouter = trpc.router().query("hello", {
+  resolve() {
+    return "hello world";
+  },
+});
 
 const app = express();
 const port = 8080;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter as any,
+    createContext: (): any => null,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
